8f3127e6c38fff0737cdf2785add7a41a9fbacf9
sagun basnet <basnetsagun8@gmail.com>
2025-09-05 13:20:51 +0545

    CRUD backend compelete

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -18,7 +18,6 @@ export const postUser = (req, res) => {
   if (!name || !phone || !email || !password) {
     return res.send({ message: "All field required" });
   }
-
   //query
   const q = "insert into user(name, phone, email, password) value(?,?,?,?)";
 
@@ -29,4 +28,42 @@ export const postUser = (req, res) => {
     }
     return res.send({ message: "Data inserted sucessfully", result });
   });
+};
+
+export const editUser = (req, res) => {
+  const id = req.params.id;
+  const { name, phone, email, password } = req.body;
+
+  //validation
+  if (!name || !phone || !email || !password) {
+    return res.send({ message: "All field required" });
+  }
+
+  const q = "update user set name=?, phone=?, email=?, password=? where id=?";
+
+  db.execute(q, [name, phone, email, password, id], (error, result) => {
+    if (error) {
+      return res.send({ message: "Error while excuteing quart", error });
+    }
+
+    return res.send({ message: "User updated sucessfully", result });
+  });
+};
+
+export const deleteUser = (req, res) => {
+  const id = req.params.id;
+
+  const q = "delete from user where id= ?";
+
+  try {
+    db.query(q, [id], (err, result) => {
+      if (err) {
+        return res.send({ message: "Error while excuteing query", err });
+      }
+
+      return res.send({ message: "User deleted sucessfully", result });
+    });
+  } catch (err) {
+    res.send({ message: "Error while running deleteUser", err });
+  }
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getUser, postUser } from "../controllers/user.js";
+import {
+  deleteUser,
+  editUser,
+  getUser,
+  postUser,
+} from "../controllers/user.js";
 
 const route = express.Router();
 
 route.get("/get-user", getUser);
 route.post("/post-user", postUser);
+route.put("/edit-user/:id", editUser);
+route.delete("/delete-user/:id", deleteUser);
 
 export default route;
